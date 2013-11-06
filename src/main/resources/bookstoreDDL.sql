69b7533a24b0aa09f0d36e01efc1199fa7dbf095
Ant10 <dozortsevanton@gmail.com>
2013-11-06 23:57:41 +0200

    Add .sql DDL and Data files

---
+++ src/main/resources/bookstoreDDL.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS BookStore;
+
+CREATE DATABASE IF NOT EXISTS BookStore;
+USE BookStore;
+
+
+CREATE TABLE Author
+(
+    Id      INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+    Name    VARCHAR(100),
+    Surname VARCHAR(100),
+    DOB     DATE
+);
+
+
+CREATE TABLE Book
+(
+    Id          INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+    Name        VARCHAR(255)           NOT NULL,
+    Author      INT                    NOT NULL,
+    Genre       VARCHAR(100),
+    Language    VARCHAR(100),
+    PubDate     DATE,
+    Pages       INT,
+    Price       DECIMAL,
+    Description LONGTEXT,
+    FOREIGN KEY (Author) REFERENCES Author (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+CREATE TABLE Client
+(
+    Id       INT PRIMARY KEY UNIQUE NOT NULL AUTO_INCREMENT,
+    Name     VARCHAR(100),
+    Surname  VARCHAR(100),
+    Email    VARCHAR(200) UNIQUE,
+    Password VARCHAR(15) UNIQUE
+);
+
+
+CREATE TABLE ClientBook
+(
+    Id       INT NOT NULL UNIQUE NOT NULL AUTO_INCREMENT,
+    IdClient INT NOT NULL,
+    IdBook   INT NOT NULL,
+    isBuy    BOOLEAN NOT NULL,
+    FOREIGN KEY (IdBook) REFERENCES Book (Id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (IdClient) REFERENCES Client (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
